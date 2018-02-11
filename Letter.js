bb9c13f61505dca369ce9b6bf1dcef703254e687
Sean Arca <sean.arca@northwestern.edu>
2018-02-11 14:55:06 -0600

    letter.js working

--- Letter.js
+++ Letter.js
@@ -1,4 +1,29 @@
 // Letter Constructor
-function Letter () {
-    
-}
+function Letter (character) {
+    this.character = character;
+    this.guess = false;
+    // Function to Show Character
+    this.showChar = function () {
+        if (this.guess) {
+            return this.character;
+        } else {
+            return ' _ ';
+        }
+    };
+    // Function to Check Character against user input
+    this.checkChar = function (input) {
+        if (input === this.character) {
+            this.guess = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+module.exports = Letter;
+
+// Test
+// var a = new Letter("A");
+// // console.log(l);
+// console.log(l.checkChar());
